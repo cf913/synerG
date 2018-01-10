376bedda5ef81394dd4a6f0a6790863c7060e4d7
Terence Chung <terencechung@hotmail.co.uk>
2018-01-10 22:28:52 +0000

    change profile.vue to match player_details.vue

--- client/src/components/center/Profile.vue
+++ client/src/components/center/Profile.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="profile">
-    <div v-if="isLoggedIn" class="container">
+    <div v-if="isLoggedIn">
       <div class="card inner-tile">
-        <div class="card-header d-flex align-items-center">
-              <img class="avatar" :src="user.img" alt="Avatar">
-              <h4 class="profile-name">{{ user.steamName }}</h4>
-              <router-link :to="{ path: `/players/${userId}/edit`}" class="profile-edit"><i class="fa fa-edit"></i></router-link>
-        </div> 
-        <div class="card-body">
-          <router-link class="btn btn-primary btn-sm" :to="{ path: `/teams/new`}"><i class="fa fa-plus fa-fw"></i> New Team</router-link>
-          <p>{{user}}</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">This is a footer</small>
+        <div class="container py-3 text-left">
+          <div class="row">
+            <div class="col-4 text-left">
+              <img class="img-thumbnails" :src="user.img" alt="Avatar">
+              <ul>
+                <li class="tiled inner-tile">
+                  <span class="title"><i class="fa fa-trophy fa-fw"></i> MMR:</span>
+                  <ul>
+                    <li>Estimate: ~{{user.mmr.mmr_estimate}}</li>
+                    <li>Solo: {{user.mmr.mmr_solo || '----'}}</li>
+                    <li>Party: {{user.mmr.mmr_party || '----'}}</li>
+                  </ul>
+                </li>
+                <li class="tiled inner-tile">
+                  <span class="title"><i class="fa fa-map-marker fa-fw"></i> Regions:</span>
+                  <ul v-for="(region, index) in user.regions" :key="index">
+                    <li>{{region}}</li>
+                  </ul>
+                </li>
+                <li class="tiled inner-tile">
+                  <span class="title"><i class="fa fa-globe fa-fw"></i> Languages:</span>
+                  <ul v-for="(language, index) in user.languages" :key="index">
+                    <li>{{language}}</li>
+                  </ul>
+                </li>
+                <li class="tiled inner-tile">
+                  <span class="title"><i class="fa fa-headphones fa-fw"></i> Comms:</span>
+                  <ul v-for="(comm, index) in user.comms" :key="index">
+                    <li>{{comm}}</li>
+                  </ul>
+                </li>
+                <li class="tiled inner-tile">
+                  <span class="title"><i class="fa fa-pie-chart fa-fw"></i> Positions:</span>
+                  <ul v-for="(position, index) in user.positions" :key="index">
+                    <li>{{position}}</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+            <div class="col-8 text-left">
+              <header class="d-flex justify-content-between align-items-center">
+                <h2>{{user.steamName}}</h2>
+                <ul class="links">
+                  <li><router-link :to="{ path: `/players/${userId}/edit`}" class="scale-up"><i class="fa fa-edit"></i></router-link></li>
+                  <li><a :href="`http://www.steamcommunity.com/profiles/${user.steamId}`" class="scale-up" target="_blank"><i class="fa fa-steam-square fa-fw"></i></a></li>
+                  <li><a href="#" class="scale-up" @click="updatePlayer"><i class="fa fa-refresh fa-fw"></i></a></li>
+                </ul>
+              </header>
+              <div class="tiled description inner-tile">
+                <p>
+                  <span class="title">Description:</span><br>
+                  {{user.description}}
+                </p>
+              </div>
+              <div class="tiled heroes inner-tile">
+                <p> 
+                  <span class="title">Most Played Heroes</span>
+                  <ul id="example-1">
+                    <li v-for="(hero, index) in user.heroes.slice(0,5)" :key="index">
+                      {{ hero }}
+                    </li>
+                  </ul>
+                </p>
+              </div>
+              <div class="tiled teams inner-tile">
+                <p class="title">Teams</p>
+              </div>
+              <div class="tiled playstyle inner-tile">
+                <p class="title">Playstyle</p>
+              </div>
+              <div class="tiled other inner-tile">
+                <p class="title">Other</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -22,6 +86,7 @@
 
   </div>
 </template>
+
 
 <script>
 export default {
@@ -34,44 +99,111 @@ export default {
     },
     isLoggedIn () {
       return true // this.$store.getters.isAuthenticated
+    },
+    inSent () {
+      if (this.$store.getters.user) return this.$store.getters.user.friends.pending_sent.includes(this.user.steamId)
+      else return false
+    },
+    inReceived () {
+      if (this.$store.getters.user) return this.$store.getters.user.friends.pending_received.includes(this.user.steamId)
+      else return false
+    },
+    inAccepted () {
+      if (this.$store.getters.user) return this.$store.getters.user.friends.accepted.includes(this.user.steamId)
+      else return false
     }
   },
   methods: {
     getSteamInfo () {
+    },
+    sendRequest (id) {
+      this.$store.dispatch('sendRequest', id)
+    },
+    updatePlayer () {
+      console.log('Updating Players...')
+      this.$store.dispatch('updatePlayer', this.$route.params.id)
     }
   }
 }
 </script>
 
 <style scoped>
-  .card {
+  
+  .tiled {
+    padding: 5px 10px;
     border-radius: 5px;
-    /* background: #000; */
-    border: 0;
     margin-bottom: 15px;
   }
-  .card-header {
+  
+  h2{
+    color: white;
+  }
+  
+  a {
+    color: white;
+  }
+  
+  header {
     border-bottom: 3px solid #666;
+    margin-bottom: 15px;
   }
-  .profile-name {
-    margin: 0;
-    flex: 4;
-  }
-
-  .profile-edit {
-    margin-top: 3px;
-    flex: 1;
-    text-align: right;
-    font-size: 1.2em;
+  
+  a.scale-up i{
+    cursor: pointer;
+    transform: scale(1);
+    transition: 0.3s;
   }
 
-  .avatar {
-    flex: 2;
-    border-radius: 3px;
-    background: #444;
-    max-width: 60px;
+  a.scale-up:hover i{
+    transform: scale(1.3);
+    transition: 0.1s;
   }
 
+  a.scale-up:active i{
+    transform: scale(1.1); 
+  }
+
+  a.scale-up:focus i{
+    transform: scale(1.1); 
+  }
+  
+  .img-thumbnails {
+    border: 3px solid #fff;
+    border-radius: 5px;
+    margin-bottom: 30px;
+    width: 100%;
+  }
+  
+  ul {
+    padding-left: 0;
+  }
+
+  li {
+    list-style-type: none;
+    line-height: 1.6em;
+    padding-left: 15px;
+  }
+
+  li.tiled {
+    margin-bottom: 10px;
+  }
+
+  ul.links {
+    margin-bottom: 5px;
+  }
+  ul.links li {
+    display: inline-block;
+  }
+  ul.links li a i {
+    font-size: 1.8em;
+  }
+  
+  .title {
+    font-weight: 600;
+    /* color: #106CD6; */
+    color: orange;
+  }
+  
   #steamBtn {
     margin: 10px auto;
   }
