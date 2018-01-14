1d882c554ab28aee1642b635e8e612712af97c5a
joeharper1000 <chaddudz@hotmail.fr>
2018-01-14 10:24:43 +1000

    pulled

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-  returnURL: 'https://synerg-tchung95.c9users.io/auth/steam/steam/return',
-  realm: 'https://synerg-tchung95.c9users.io/',
-  // returnURL: 'http://localhost:3000/auth/steam/steam/return',
-  // realm: 'http://localhost:3000/',
+  // returnURL: 'https://synerg-tchung95.c9users.io/auth/steam/steam/return',
+  // realm: 'https://synerg-tchung95.c9users.io/',
+  returnURL: 'http://localhost:3000/auth/steam/steam/return',
+  realm: 'http://localhost:3000/',
   apiKey: 'E42F3C60E348D57D431B6B60F5B7E3C0'
 }
