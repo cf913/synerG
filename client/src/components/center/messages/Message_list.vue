623f1925111dd7c04758c9b99d774b5e91f812e1
Terence Chung <terencechung@hotmail.co.uk>
2018-02-13 19:02:32 +0000

    message list now displays the profile picture of recipient

--- client/src/components/center/messages/Message_list.vue
+++ client/src/components/center/messages/Message_list.vue
@@ -3,10 +3,19 @@
     <div>
       <ul class="list-group clearfix" v-for="(conversation, index) in conversations" :key="index">
         <li class="list-group-item inner-tile">
-          <span v-for="(participant, index) in conversation.conversationId.participants" :key="index">
-            <h4 v-if="participant.steamName !== user.steamName">{{participant.steamName}} </h4>
-          </span>
-          <p>{{conversation.author.steamName}}: {{conversation.body}}</p>
+          <div class="row">
+            <div class="col-sm-2 image">
+              <span v-for="(participant, index) in conversation.conversationId.participants" :key="index">
+                <img v-if="participant.steamName !== user.steamName" id="avatar" :src="participant.img" alt="Avatar">
+              </span>
+            </div>
+            <div class="col-sm-10">
+              <span v-for="(participant, index) in conversation.conversationId.participants" :key="index">
+                <h4 v-if="participant.steamName !== user.steamName">{{participant.steamName}} </h4>
+              </span>
+              <p>{{conversation.author.steamName}}: {{conversation.body}}</p>
+            </div>
+          </div>
         </li>
       </ul>
     </div>
@@ -40,5 +49,15 @@ export default {
     padding: 5px 10px;
     border-radius: 5px;
     margin-bottom: 15px;
+  }
+  
+  #avatar {
+    width: 100%;
+    border: 3px solid #fff;
+    border-radius: 5px;
+  }
+  
+  li .image {
+    padding: 0;
   }
 </style>
