cebe81d4c0c32edeb1e27ef6bb23d374de604329
unknown <terencechung@hotmail.co.uk>
2018-02-22 14:11:32 +0000

    add post input form to news.vue

--- client/src/components/center/news/News.vue
+++ client/src/components/center/news/News.vue
@@ -1,17 +1,35 @@
 <template>
-    <div class="players tile gray-tile tile-shadow">
+    <div class="news-feed text-left container tile gray-tile tile-shadow">
         <h2 class="py-3">News</h2>
+        <form>
+        <div class="row">
+        	<div class="left col-lg-3">
+        		<img class="avatar" :src="user.img" alt="Avatar">
+        	</div>
+        	<div class="right col-lg-9">
+        		<label class="col-form-label" for="post">Post:</label>
+          	<input type="text" class="form-control" id="post" name="post" v-model="post" placeholder="Write Post ...">
+          	<button>Submit</button>
+        	</div>
+        </div>
+      </form>
     </div>
 </template>
 
 <script>
 export default {
-
+	data: () => {
+		return {
+			post: ''
+		}
+	},
+	computed: {
+		user () {
+			return !this.$store.getters.user ? 'No user logged in' : this.$store.getters.user
+		}
+	}
 }
 </script>
 
 <style scoped>
-    .players {
-        color: white;
-    }
 </style>
