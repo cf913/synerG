9bc4ebe508ea0efcf9f170a0f6335b2966939065
unknown <terencechung@hotmail.co.uk>
2018-04-28 19:33:43 +0100

    only community members can see posts inside the community

--- client/src/components/center/communities/Community_posts.vue
+++ client/src/components/center/communities/Community_posts.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="community_posts tile gray-tile">
-    <div class="tiled description inner-tile">
-      <form v-if="isCommunityMember || isCommunityAdmin">
+    <div v-if="isCommunityMember || isCommunityAdmin" class="tiled description inner-tile">
+      <form>
         <label class="col-form-label title" for="communityPost">Write a Post:</label>
         <input type="text" class="form-control" id="communityPost" name="communityPost" v-model="communityPost" placeholder="Write Post ...">
         <div class="d-flex justify-content-end align-items-center">
@@ -9,7 +9,7 @@
         </div>
       </form>
     </div>
-    <ul class="list-group clearfix">
+    <ul v-if="isCommunityMember || isCommunityAdmin" class="list-group clearfix">
       <li class="list-group-item inner-tile" v-for="(post, index) in communityPosts" :key="index">
         <div class="row">
           <div class="left col-lg-3">
@@ -22,6 +22,7 @@
         </div>
       </li>
     </ul>
+    <h6 v-else class="title">Please join the community in order to see posts inside this community</h6>
   </div>
 </template>
 
